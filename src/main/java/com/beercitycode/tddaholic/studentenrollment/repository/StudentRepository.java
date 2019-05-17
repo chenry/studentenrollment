eb4fcb43ecd89d34dfb43accd875424eac69d9e6
Carlus Henry <carlushenry@kpmg.com>
2019-05-17 12:21:26 -0400

    stopping with jdbctemplate dao and moving forward with repository approach

--- src/main/java/com/beercitycode/tddaholic/studentenrollment/repository/StudentRepository.java
+++ src/main/java/com/beercitycode/tddaholic/studentenrollment/repository/StudentRepository.java
@@ -1,9 +1,13 @@
 package com.beercitycode.tddaholic.studentenrollment.repository;
 
 import com.beercitycode.tddaholic.studentenrollment.model.Student;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,6 +18,17 @@ public class StudentRepository {
 
   public Student findStudentById(UUID id) {
     String query = String.format("select id, first_name, last_name from student where id = '%s'", id);
-    return jdbcTemplate.queryfor
+    return jdbcTemplate.query(query, new RowMapper<Student>() {
+
+      @Override
+      public Student mapRow(ResultSet resultSet, int i) throws SQLException {
+
+        Student s = new Student();
+        s.setId(UUID.fromString(resultSet.getString("id")));
+        s.setLastName(resultSet.getString("last_name"));
+        s.setFirstName(resultSet.getString("first_name"));
+        return s;
+      }
+    });
   }
 }
