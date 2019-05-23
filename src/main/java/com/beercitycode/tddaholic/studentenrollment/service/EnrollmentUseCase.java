bce8f5fce0181495dab266e48f887048a85abbf0
Carlus Henry <carlushenry@gmail.com>
2019-05-22 20:51:22 -0400

    changing name of class.

--- src/main/java/com/beercitycode/tddaholic/studentenrollment/service/EnrollmentUseCase.java
+++ src/main/java/com/beercitycode/tddaholic/studentenrollment/service/EnrollmentUseCase.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 @Service
-public class StudentEnrollmentService {
+public class EnrollmentUseCase {
 
     @Autowired
     private StudentService studentService;
